57ee9fc0a0f8d692e91beae8f7268a0ea80a5d6e
yada2x <lawrenceltnguyen@gmail.com>
2025-01-16 16:36:58 +1100

    i forgot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -27,5 +27,30 @@ for (const folder of commandFolders) {
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    
+    console.log(interaction); // temp
+
+    command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: `There was an error while executing this command!`, flags: MessageFlags.Ephemeral});
+        } else {
+            await interaction.reply({ content: `There was an error while executing this command!`, flags: MessageFlags.Ephemeral});
+        }
+    }
+})
 
 client.login(token);
