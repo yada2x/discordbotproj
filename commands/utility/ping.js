0ec42c92ecc5166e85454f536f1583f2d54c8044
yada2x <lawrenceltnguyen@gmail.com>
2025-01-16 18:34:40 +1100

    Separate event listeners from index.js and update ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,7 +5,8 @@ module.exports = {
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        const sent = await interaction.reply({content: 'Pinging...', fetchReply: true });
+        interaction.editReply(`Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
     },
 };
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
 
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
@@ -24,33 +25,17 @@ for (const folder of commandFolders) {
     }
 }
 
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    console.log(interaction); // temp
-
-    command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: `There was an error while executing this command!`, flags: MessageFlags.Ephemeral});
-        } else {
-            await interaction.reply({ content: `There was an error while executing this command!`, flags: MessageFlags.Ephemeral});
-        }
-    }
-})
 
 client.login(token);
